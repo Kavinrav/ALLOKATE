28ad982e2946ca46d706de3d4abf9c092483c403
Kavinrav <rajkavin237@gmail.com>
2026-04-15 22:14:32 +0530

    Refactor: Move loop variable declarations outside for statements for C89 compatibility

--- main.c
+++ main.c
@@ -26,8 +26,9 @@ int visited[MAX_STUDENTS];
 // Function to count common interests
 int commonInterests(Student s1, Student s2) {
     int count = 0;
-    for (int i = 0; i < s1.interestCount; i++) {
-        for (int j = 0; j < s2.interestCount; j++) {
+    int i, j;
+    for (i = 0; i < s1.interestCount; i++) {
+        for (j = 0; j < s2.interestCount; j++) {
             if (strcmp(s1.interests[i], s2.interests[j]) == 0) {
                 count++;
             }
@@ -53,7 +54,8 @@ void dfs(int node) {
     visited[node] = 1;
     printf("%s ", students[node].name);
 
-    for (int i = 0; i < n; i++) {
+    int i;
+    for (i = 0; i < n; i++) {
         if (graph[node][i] && !visited[i]) {
             dfs(i);
         }
@@ -62,14 +64,15 @@ void dfs(int node) {
 
 // Build similarity graph
 void buildGraph() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
             graph[i][j] = 0;
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
             if (isSimilar(students[i], students[j])) {
                 graph[i][j] = graph[j][i] = 1;
             }
@@ -82,7 +85,8 @@ void inputStudents() {
     printf("Enter number of students: ");
     scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
+    int i, j;
+    for (i = 0; i < n; i++) {
         students[i].id = i;
 
         printf("\nStudent %d name: ", i + 1);
@@ -94,7 +98,7 @@ void inputStudents() {
         printf("Number of interests: ");
         scanf("%d", &students[i].interestCount);
 
-        for (int j = 0; j < students[i].interestCount; j++) {
+        for (j = 0; j < students[i].interestCount; j++) {
             printf("Interest %d: ", j + 1);
             scanf("%s", students[i].interests[j]);
         }
@@ -103,13 +107,14 @@ void inputStudents() {
 
 // Print groups
 void findGroups() {
-    for (int i = 0; i < n; i++) {
+    int i;
+    for (i = 0; i < n; i++) {
         visited[i] = 0;
     }
 
     printf("\nStudy Groups:\n");
 
-    for (int i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         if (!visited[i]) {
             printf("Group: ");
             dfs(i);
